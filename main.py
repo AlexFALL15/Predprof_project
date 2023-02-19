84da2333638897b27744c3a99d12e1b269e16d8c
AlexFALL15 <119256732+AlexFALL15@users.noreply.github.com>
2023-02-19 20:52:56 +0300

    версия 1

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import sys
-import os
-from graph_ui import UiForm
-from database import *
-from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog, QMessageBox, QTableWidgetItem
-from PyQt5.QtCore import QTimer
-from datetime import datetime
-import requests
 import json
+import sys
+from datetime import datetime
+
+import requests
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog, QMessageBox
 from bs4 import BeautifulSoup
 
+from database import *
+from graph_ui import UiForm
 
-URLSTART = 'http://192.168.137.122:5000/Start'
-URLSTOP = 'http://127.0.0.1:5000/Stop'
+URLSTART = 'http://esp8266.local/start'
+URLSTOP = 'http://esp7266.local/dowload'
 
 
 class Window(QMainWindow, UiForm):
@@ -28,13 +28,14 @@ class Window(QMainWindow, UiForm):
             "Battle for the 3rd place",
             "Final"
         ]
-        self.folder = '/stats/'
         self.current_race = ''
         d = datetime.now()
         self.date = f'{d.year}-{d.month}-{d.day}'
-        self.json = {i: None for i in self.race_type}
-        # self.req = requests.get(URL)
+        self.json = {}
         self.comp = True
+        self.racer = ''
+        self.racers = []
+        self.label_4.setText(self.date)
 
         self.pushButton.clicked.connect(self.make_race)
         self.pushButton.setDisabled(True)
@@ -44,56 +45,74 @@ class Window(QMainWindow, UiForm):
         self.pushButton_3.setDisabled(True)
         self.pushButton_4.clicked.connect(self.make_comp)
         self.pushButton_5.clicked.connect(self.un_log_in)
-        self.select_data()
-    
+        self.pushButton_6.clicked.connect(self.table)
+        self.pushButton_6.setDisabled(True)
+
     def make_comp(self):
-         if self.lineEdit.text() == '' or self.lineEdit_2.text() == '':
-             QMessageBox.critical(self, "Ошибка ", "Не веденно название соревнования или организатор соревнования",
-                                  QMessageBox.Ok)
-         else:
+        if self.lineEdit.text() == '' or self.lineEdit_2.text() == '' or self.racer_1.text() == '' or self.racer_2.text() == '':
+            QMessageBox.critical(self, "Ошибка ",
+                                 "Не веденно название соревнования или организатор соревнования или не указан один из номеров гонщиков",
+                                 QMessageBox.Ok)
+        else:
             self.lineEdit.setDisabled(True)
             self.lineEdit_2.setDisabled(True)
+            self.racer_1.setDisabled(True)
+            self.racer_2.setDisabled(True)
             self.pushButton_4.setDisabled(True)
+            self.pushButton_6.setDisabled(False)
             self.pushButton.setDisabled(False)
             self.setWindowTitle(self.lineEdit.text())
+            self.racers = [self.racer_1.text(), self.racer_2.text()]
+            self.label_8.setText(f'{self.racers[0]} - w')
+            self.label_9.setText(f'{self.racers[1]} - r')
             if not check_in(self.lineEdit.text, self.lineEdit_2.text(), self.date, f'{self.lineEdit.text()}.json'):
                 make_stroke(self.lineEdit.text(), self.lineEdit_2.text(), self.date, f'{self.lineEdit.text()}.json')
-                self.select_data()
             self.comp = False
             try:
-                with open(self.folder + f'{self.lineEdit.text()}.json') as f:
+                with open(f'{self.lineEdit.text()}.json') as f:
                     self.json = json.load(f)
             except Exception:
-                self.json = {i: None for i in self.race_type}
+                self.json = {self.racer_1.text(): {i: None for i in self.race_type},
+                             self.racer_2.text(): {i: None for i in self.race_type}}
 
     def un_log_in(self):
         self.lineEdit.setDisabled(False)
         self.lineEdit_2.setDisabled(False)
+        self.racer_1.setDisabled(False)
+        self.racer_2.setDisabled(False)
         self.pushButton_4.setDisabled(False)
         self.pushButton_2.setDisabled(True)
         self.pushButton_3.setDisabled(True)
+        self.pushButton_6.setDisabled(True)
         self.setWindowTitle('Новое соревнования')
         self.comp = True
         self.current_race = ''
         self.json = {i: None for i in self.race_type}
-        
-    
+
     def make_race(self):
+        self.select_racer()
         race_type, ok_pressed = QInputDialog.getItem(
             self, "ВЫБОР", "Выберете тип гонки",
             self.race_type, 1, False)
         if ok_pressed:
             self.current_race = race_type
-            if self.json[self.current_race] is not None:
+            self.label_7.setText(f'{self.label_7.text()} {self.current_race}')
+            if self.json[self.racer][self.current_race] != '':
                 self.pushButton_2.setDisabled(True)
                 self.pushButton_3.setDisabled(False)
             else:
                 self.pushButton_3.setDisabled(True)
                 self.pushButton_2.setDisabled(False)
 
-    
+    def select_racer(self):
+        racer, ok_pressed_1 = QInputDialog.getItem(
+            self, "ВЫБОР", "Выберите гонщика для измерения",
+            self.racers, 1, False)
+        if ok_pressed_1:
+            self.racer = racer
+
     def start(self):
-        if self.json[self.current_race] is None:
+        if self.json[self.racer][self.current_race] is None:
             self.pushButton_2.setDisabled(True)
             time = int(self.label_3.text())
             requests.get(URLSTART)
@@ -101,40 +120,39 @@ class Window(QMainWindow, UiForm):
                 self.label_3.setText(f"{time - 1}")
                 QTimer().singleShot(1000, self.start)
             else:
-                self.label_3.setText('60')
+                self.label_3.setText('30')
                 page = requests.get(URLSTOP)
                 soup = BeautifulSoup(page.text, 'html.parser')
-                sp = soup.findAll('tr')
-                d = []
-                for i in sp:
-                    for j in i.text.split('12544'):
-                        if len(j.split('-')) == 3:
-                            d.append(list(map(int, j.split('-'))))
-                        else:
-                            d.append(list(map(int, j.split('-')[1:])))
-                self.json[self.current_race] = d
-                with open(self.folder + f'{self.lineEdit.text()}.json', 'w+') as f:
+                d = list(map(int, soup.findAll('br')))
+                self.json[self.racer][self.current_race] = d
+                with open(f'{self.lineEdit.text()}.json', 'w+') as f:
                     json.dump(self.json, f)
                 self.pushButton_3.setDisabled(False)
 
     def graph(self):
-        with open(self.folder + f'{self.lineEdit.text()}.json', 'r') as f:
+        if self.current_race != 'Qualification':
+            QMessageBox.critical(self, "Ошибка ",
+                                 "Не сделано замеров второго гонщика",
+                                 QMessageBox.Ok)
+            self.racers.pop(self.racers.index(self.racer))
+            self.select_racer()
+            self.start()
+            self.racers = [self.racer_1.text(), self.racer_2.text()]
+        with open(f'{self.lineEdit.text()}.json', 'r') as f:
             data = json.load(f)
-        self.graphicsView.plot([i[0] for i in data[self.current_race]], [i for i in range(1, 7)], pen='w')
+        if self.current_race == 'Qualification':
+            self.graphicsView.plot([i for i in range(len(data[self.racer][self.current_race]))],
+                                   [i for i in data[self.racer][self.current_race]], pen='w')
+        else:
+            self.graphicsView.plot([i for i in range(len(data[self.racer][self.current_race]))],
+                                   [i for i in data[self.racer_1.text()][self.current_race]], pen='w')
+            self.graphicsView.plot([i for i in range(len(data[self.racer][self.current_race]))],
+                                   [i for i in data[self.racer_2.text()][self.current_race]], pen='r')
 
-    def select_data(self):
-        query = "SELECT * FROM comps"
-        res = sqlite3.connect('competitions.db').cursor().execute(query).fetchall()
-        self.tableWidget.setColumnCount(5)
-        self.tableWidget.setRowCount(0)
-        for i, row in enumerate(res):
-            self.tableWidget.setRowCount(
-                self.tableWidget.rowCount() + 1)
-            for j, elem in enumerate(row):
-                self.tableWidget.setItem(
-                    i, j, QTableWidgetItem(str(elem)))
-    
-    
+    def table(self):
+        pass
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
